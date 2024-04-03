769a7f1de83db4d6042f7de51df9816f3d16f92d
Lucaalex00 <lu.al.cirio00@gmail.com>
2024-04-03 12:06:01 +0200

    add Milestone 1 + add NullChecker method Commit

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -11,20 +11,69 @@ export default {
     methods: {
         getUserInput() {
             console.log(this.userInput)
-        }   
+            this.userInput = ''
+        },
+        getMovies() {
+            this.productCallState.getProducts(`${this.productCallState.products_url}movie?api_key=${this.productCallState.api_key}&query=${this.userInput}`)
+        },
+        nullChecker(el) {
+            if (el != null) {
+                return el;
+            } else {
+                return "-";
+            }
+            
+        }
     },
-
+    mounted() {
+        this.getMovies()
+    }
+    
 }
 </script>
 <template>
     <div>
-        <input type="text" placeholder="Inserisci titolo" v-model="userInput">
-        <button @click="getUserInput"> Conferma </button>
+        <input type="text" placeholder="Inserisci titolo" v-model="userInput" @keyup.enter="getMovies">
+        <button @click="getMovies"> Conferma </button>
+        <div v-if="productCallState.products.length>0">
+            <div class="my-2 p-2 flex-col gap-2 border-2" v-for="product in productCallState.products">
+                <div v-if="product.title==product.original_title">Title:{{ nullChecker(product.title) }}</div>
+                <div class="flex flex-col" v-else>
+                    <span v-if="product.title!=null">Title: {{ nullChecker(product.title) }} </span>
+                    <span>Original Title: {{ nullChecker(product.original_title) }}</span>
+                </div>
+                <div>Description: {{ nullChecker(product.overview) }}</div>
+                <div>Language: "{{ nullChecker(product.original_language) }}"</div>
+                <div>Vote: {{ nullChecker(product.vote_average)}}</div>
+            </div>
+        </div>
 
     </div>
 </template>
 
 
-<style lang="scss">
+<style>
+    .flex{
+        display: flex;
+    }
 
+    .flex-col{
+        flex-direction:column;
+    }
+
+    .gap-2{
+        gap:1rem;
+    }
+
+    .border-2{
+        border:2px solid black;
+    }
+
+    .p-2{
+        padding:0.5rem
+    }
+
+    .my-2{
+        margin:4px 0
+    }
 </style>
--- src/store/apiCallState.js
+++ src/store/apiCallState.js
@@ -2,12 +2,13 @@ import { reactive } from "vue"; //IMPORT REACTIVE FROM VUEJS
 import axios from "axios"; //Import Axios to Get CALLS
 
 export const productCallState = reactive({
-  products_url: "https://api.themoviedb.org/3/search/movie",
+  products_url: "https://api.themoviedb.org/3/search/",
+  api_key: "a7a4b4caa5e08038dbfe54a434870657",
   products: [],
   getProducts(url) {
     axios.get(url).then((response) => {
       console.log(response);
-      this.products = response.data;
+      this.products = response.data.results;
     });
   },
 });
